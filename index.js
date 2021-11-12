45be9f1a01db510066445cb7c2e423f3f6811bc1
tom maenhout <tom.maenhout@gmail.com>
2021-11-12 20:32:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+
+app.use(cors())
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["*"]
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("message", message => {
+        console.log(message)
+        io.emit("message",message)
+    })
+    
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
